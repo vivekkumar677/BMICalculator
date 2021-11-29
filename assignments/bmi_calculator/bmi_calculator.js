5fdb66cb5979a393c85573b51314e9ea89cc25fe
Vivek Kumar <ervivekkumar677@gmail.com>
2021-11-29 14:51:27 +0530

    bmi_calculator assignment submission

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,6 +1,12 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
     // Write your code here
-}
-
+    if (height>0 && mass>0){
+        BMI = mass / (height * height)
+        return BMI;
+    } else{
+        return "INVALID INPUT";
+    }
+};
 module.exports = BMICalculator;
+console.log(BMICalculator());
--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -1,7 +1,14 @@
 const movies = [];
 
-function favouriteMovie(operation, movie) {
+function favouriteMovie(operation="add", movie="lucy") {
     // Write your code here
-}
+
+    if (operation === "add"){
+        return movies.push(movie);
+    } else if(operation === "remove"){
+        return movies.pop(movie);
+    }
+};
 
 module.exports = favouriteMovie;
+console.log(favouriteMovie());
